090784bd6471d232efb8e674bbe689f64b6727b7
Jacques Légaré <jacques.legare70@gmail.com>
2021-04-28 16:37:04 -0400

    Handle optional types.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -53,8 +53,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
 fn assign_field(field: &syn::Field) -> proc_macro2::TokenStream {
     let field_name = &field.ident;
-    quote! {
-        #field_name: self.#field_name.clone().ok_or(format!("{} is not set.", stringify!(#field_name)))?
+    match unwrap_inner_type(&field.ty, "Option") {
+        Some(_) => quote! { #field_name: self.#field_name.clone() },
+        None => quote! {
+            #field_name: self.#field_name.clone().ok_or(format!("{} is not set.", stringify!(#field_name)))?
+        },
     }
 }
 
@@ -68,14 +71,54 @@ fn initialize_field(field: &syn::Field) -> proc_macro2::TokenStream {
 fn optionize_struct_field(field: &syn::Field) -> proc_macro2::TokenStream {
     let field_name = &field.ident;
     let field_type = &field.ty;
-    quote! { #field_name: std::option::Option<#field_type> }
+    match unwrap_inner_type(field_type, "Option") {
+        Some(_) => quote! { #field_name: #field_type },
+        None => quote! { #field_name: std::option::Option<#field_type> },
+    }
 }
 
 fn methodize_struct_field(field: &syn::Field) -> proc_macro2::TokenStream {
     let field_name = &field.ident;
     let field_type = &field.ty;
-    quote! { pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
-        self.#field_name = Some(#field_name);
-        self
-    } }
+    match unwrap_inner_type(&field_type, "Option") {
+        Some(inner_type) => {
+            quote! { pub fn #field_name(&mut self, #field_name: #inner_type) -> &mut Self {
+                self.#field_name = Some(#field_name);
+                self
+            } }
+        }
+        None => quote! { pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+            self.#field_name = Some(#field_name);
+            self
+        } },
+    }
+}
+
+fn unwrap_inner_type<'a>(field_type: &'a syn::Type, wrapper: &str) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = field_type
+    {
+        if let std::option::Option::Some(syn::PathSegment {
+            ident,
+            arguments:
+                syn::PathArguments::AngleBracketed(
+                    syn::AngleBracketedGenericArguments { args: types, .. },
+                    ..,
+                ),
+            ..
+        }) = segments.first()
+        {
+            if ident == wrapper {
+                if let std::option::Option::Some(syn::GenericArgument::Type(inner_type)) =
+                    types.first()
+                {
+                    return std::option::Option::Some(inner_type);
+                }
+            }
+        }
+    }
+
+    None
 }
