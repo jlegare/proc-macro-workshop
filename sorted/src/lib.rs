eb14f5e8ef973b6cce3aff855094ff91a02a1000
Jacques Légaré <jacques.legare70@gmail.com>
2021-05-04 13:58:25 -0400

    The first test passes.

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,13 @@
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let tree = parse_macro_input!(input as syn::DeriveInput);
 
-    unimplemented!()
+    (quote! { #tree }).into()
 }
