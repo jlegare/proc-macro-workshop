285690c0dcbba674496b9b049d69742d891fe124
Jacques Légaré <jacques.legare70@gmail.com>
2021-05-07 16:22:01 -0400

    The second test passes.

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -9,5 +9,15 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let tree = parse_macro_input!(input as syn::DeriveInput);
 
-    (quote! { #tree }).into()
+    parse_sorted(tree).unwrap_or_else(|e| e.to_compile_error().into())
+}
+
+fn parse_sorted(tree: syn::DeriveInput) -> Result<TokenStream, syn::Error> {
+    match tree.data {
+        syn::Data::Enum(_) => Ok((quote! { #tree }).into()),
+        _ => Err(syn::parse::Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        )),
+    }
 }
