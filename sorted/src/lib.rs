1376dd0f5bb724ee26f1f305fbb591351246c1b1
Jacques Légaré <jacques.legare70@gmail.com>
2021-05-10 13:48:00 -0400

    The third test passes.

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -9,17 +9,37 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let tree = parse_macro_input!(input as syn::DeriveInput);
 
-    eprintln!("{:#?}", tree);
-
     parse(tree).unwrap_or_else(|e| e.to_compile_error().into())
 }
 
 fn parse(tree: syn::DeriveInput) -> Result<TokenStream, syn::Error> {
-    match tree.data {
-        syn::Data::Enum(_) => Ok((quote! { #tree }).into()),
+    match &tree.data {
+        syn::Data::Enum(data) => {
+            check_sorted(&data.variants)?;
+            Ok((quote! { #tree }).into())
+        }
         _ => Err(syn::parse::Error::new(
             proc_macro2::Span::call_site(),
             "expected enum or match expression",
         )),
     }
+}
+
+fn check_sorted(
+    variants: &syn::punctuated::Punctuated<syn::Variant, syn::token::Comma>,
+) -> Result<(), syn::Error> {
+    for outer in variants.iter() {
+        for inner in variants.iter() {
+            if outer.ident == inner.ident {
+                break;
+            } else if outer.ident < inner.ident {
+                return Err(syn::parse::Error::new(
+                    outer.ident.span(),
+                    format!("{} should sort before {}", outer.ident, inner.ident),
+                ));
+            }
+        }
+    }
+
+    Ok(())
 }
